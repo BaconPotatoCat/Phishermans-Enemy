cbcc0ff056e673e940b737ffce55ac9643df8c9e
Jun Wei <53926665+junwei2012@users.noreply.github.com>
2022-02-07 14:30:38 +0800

    Add files via upload
    Added prediction function of python with a sample code inside on how it works a list of dictionary

---
+++ Model/prediction.py
@@ -0,0 +1,29 @@
+import pickle
+import pandas as pd
+
+
+columns = ["LongURL", "ShortURL", "SubDomains", "HTTPS", "RequestURL", "AnchorURL", "ServerFormHandler", "UsingPopupWindow", "AgeofDomain", "WebsiteTraffic"]
+
+
+model1 = 1
+listOfDict1 = [{"LongURL": 1, "ShortURL": 1, "SubDomains": 0, "HTTPS": -1,"RequestURL": 1, "AnchorURL": 0, "ServerFormHandler": -1, "UsingPopupWindow": 1, "AgeofDomain": -1,"WebsiteTraffic": 0}, {"LongURL": 1, "ShortURL": 1, "SubDomains": 0, "HTTPS": 1,"RequestURL": 1, "AnchorURL": 0, "ServerFormHandler": -1, "UsingPopupWindow": 1, "AgeofDomain": -1,"WebsiteTraffic": 0}]
+
+
+def predict(model, listOfDict):
+    if model == 1:
+        val_x = pd.DataFrame.from_dict(listOfDict, orient='columns')
+        GBC = pickle.load(open("train_GBC_model.sav", 'rb'))
+        z = GBC.predict(val_x)
+        return z
+    else:
+        return "Error in model selection"
+
+
+def main():
+    x = predict(model1, listOfDict1)
+    for i in x:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
